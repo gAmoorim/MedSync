e6fc10914b19dbbe45f0eeabb45a345cc0cdf8b4
gamoorim <gabrielrock012@gmail.com>
2026-06-28 17:25:20 -0300

    rota para agendar e cancelar consulta

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { controllerLoginUsuario } = require('../controllers/controllerLogin')
-const { controllerCriarPaciente, controllerPerfilPaciente, controllerAtualizarPaciente, controllerAlterarSenhaPaciente, controllerHorariosDisponiveis, controllerAgendarConsulta } = require('../controllers/controllerPaciente')
+const { controllerCriarPaciente, controllerPerfilPaciente, controllerAtualizarPaciente, controllerAlterarSenhaPaciente, controllerHorariosDisponiveis, controllerAgendarConsulta, controllerCancelarConsultaPaciente } = require('../controllers/controllerPaciente')
 
 const auth = require('../middlewares/auth')
 
@@ -14,6 +14,7 @@ routers.get('/pacientes/perfil', auth, controllerPerfilPaciente)
 routers.put('/pacientes/perfil', auth, controllerAtualizarPaciente)
 routers.put('/pacientes/senha', auth, controllerAlterarSenhaPaciente)
 routers.get('/pacientes/horarios-disponiveis', auth, controllerHorariosDisponiveis)
-routers.post('/pacientes/consultas', auth, controllerAgendarConsulta)
+routers.post('/pacientes/consultas', auth, controllerAgendarConsulta) //ENVIAR EMAIL CONFIRMANDO CONSULTA
+routers.put('/pacientes/consultas/:consulta_id/cancelar', auth, controllerCancelarConsultaPaciente) // Enviar e-mail de confirmação de cancelamento ao paciente
 
 module.exports = routers
